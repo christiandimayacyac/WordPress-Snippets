4180d45448cb6728c9fed205abd09930295b717d
christiandimayacyac <christiandimayacyac@gmail.com>
2018-07-20 21:17:32 +0800

    added nav_walker.php for menu rendering

---
+++ header_php/nav_walker.php
@@ -0,0 +1,8 @@
+<?php
+    wp_nav_menu( array(
+        'theme_location'    => 'primary-menu',
+        'container'         => 'nav',
+        'container_class'   => 'navbar-collapse collapse',
+        'menu_class'        => 'nav navbar-nav navbar-right'
+    ) );
+?>
