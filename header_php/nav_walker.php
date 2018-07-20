826f4d76accd614d89b998e92b23288a3e600694
christiandimayacyac <christiandimayacyac@gmail.com>
2018-07-20 21:55:52 +0800

    added socials menu in functions.php snippet for registering menus

--- header_php/nav_walker.php
+++ header_php/nav_walker.php
@@ -5,4 +5,15 @@
         'container_class'   => 'navbar-collapse collapse',
         'menu_class'        => 'nav navbar-nav navbar-right'
     ) );
+    
+    wp_nav_menu( array(
+        'theme_location'    => 'socials-menu',
+        'container'         => 'nav',
+        'container_class'   => 'socials',
+        'container_id'      => 'socials',
+        'link_before'       => '<span class="sr-only"',
+        'link_after'        => '</span>'
+    ) );
 ?>
+
+// Social Icons are displayed while anchor tag text is hidden
