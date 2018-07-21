d48bb5ab5f49d57a03b6c5f42212902c36fa6c55
christiandimayacyac <christiandimayacyac@gmail.com>
2018-07-21 10:21:38 +0800

    inserted script loading in enqueue_css_files

--- functions_php/enque_css_files.php
+++ functions_php/enque_css_files.php
@@ -1,6 +1,23 @@
 <?
 
 function themeName_styles() {
+    //adding scripts
+    wp_register_script('jquery', get_template_directory_uri(). '/js/jquery3.2.2.js', array(), '8.0.0', true );
+    wp_register_script('script', get_template_directory_uri(). '/js/main.js', array(), '1.0.0', true );
+
+    //enque the script(s)
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('script');
+}
+
+//hook the enque
+add_action('wp_enqueue_scripts', 'themeName_styles');
+
+
+// ------above code could be added together with the stylesheets------ //
+<?
+
+function themeName_styles() {
     //adding stylesheets
     wp_register_style('normalize', get_template_directory_uri(). '/css/normalize.css', array(), '8.0.0' );
     wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0.0' );
@@ -8,6 +25,14 @@ function themeName_styles() {
     //enque the style(s)
     wp_enqueue_style('normalize');
     wp_enqueue_style('style');
+    
+    //adding scripts
+    wp_register_script('jquery', get_template_directory_uri(). '/js/jquery3.2.2.js', array(), '8.0.0', true );
+    wp_register_script('script', get_template_directory_uri(). '/js/main.js', array(), '1.0.0', true );
+
+    //enque the script(s)
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('script');
 }
 
 //hook the enque
